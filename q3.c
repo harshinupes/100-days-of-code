a5e09539488c713afabdbd528c92cc341b7b2362
harshinupes <Harshdeep.31640@stu.upes.ac.in>
2026-09-18 17:21:50 +0530

    Delete q3.c

--- q3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    int l,b,area;
-    printf("enter lenght and breadth \n");
-    scanf("%d %d",&l,&b);
-    area=l*b;
-    printf("area of rectangle is %d\n",area);
-    return 0;
-}
