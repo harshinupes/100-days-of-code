ddedb9a3d8fec3076770c9a08930ba37293a53ce
harshinupes <Harshdeep.31640@stu.upes.ac.in>
2026-09-22 18:51:51 +0530

    day 2

--- day 2/q3.c
+++ day 2/q3.c
@@ -1,3 +1,24 @@
+/*
+*NAME:HARSHDEEP NAGAR
+*SAP ID:590031640
+*DATE:18/09/2026
+*PROBLEM STATEMENT
+Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
+/*
+Sample Test Cases:
+Input 1:
+5 10
+Output 1:
+Area=50, Perimeter=30
+
+Input 2:
+3 7
+Output 2:
+Area=21, Perimeter=20
+
+
+*/
 #include<stdio.h>
 int main() {
     int l,b,area;
--- day 2/q4.c
+++ day 2/q4.c
@@ -1,11 +1,32 @@
+/*
+*NAME:HARSHDEEP NAGAR
+*SAP ID:590031640
+*DATE:18/09/2026
+*PROBLEM STATEMENT
+Q4: Write a program to calculate the area and circumference of a circle given its radius.
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+*/
 #include <stdio.h>
+
 int main() {
-    float circumference,radius,area;
-    printf("enter radius \n");
-    scanf("%f",&radius);
-area=3.14*radius*radius;
-circumference=2*3.14*radius;
-printf("area of circle is %f\n",area);
-printf("circumference of circle is %f\n",circumference);
-return 0;
+    float radius, area, circumference;
+    float PI = 3.14;
+
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    printf("Area=%.2f, Circumference=%.2f\n", area, circumference);
+    return 0;
 }
