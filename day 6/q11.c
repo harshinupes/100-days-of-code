1f28ed449051d1ca98777739c97a0604dad5298f
harshinupes <Harshdeep.31640@stu.upes.ac.in>
2026-09-25 00:00:36 +0530

    day 6

---
+++ day 6/q11.c
@@ -0,0 +1,40 @@
+/* 
+NAME:HARSHDEEP NAGAR
+SAP ID: 590031640
+BTACH: CSE-59
+
+Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+#include<stdio.h>
+int main()
+{ 
+    int num;
+     printf("Enter an number: ");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("%d is even",num);
+    }
+    else
+    {
+        printf("%d is odd",num);
+    }
+    return 0;
+}
+
+
+
+
